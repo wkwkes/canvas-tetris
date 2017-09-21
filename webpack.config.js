77f8de7ea6315485b6d81d4862e7365b02665e8a
wkwkes <wkwkes@gmail.com>
2017-09-21 18:21:40 +0900

    remane folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./lib/js/ocaml/app.js",
+  entry: "./lib/js/src/app.js",
   output: {
     filename: "bundle.js",
     path: path.join(__dirname, "lib/js")
